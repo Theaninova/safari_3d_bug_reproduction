310620071cd5d255cc3eb00b574f62770c16c97d
Thea Schöbl <dev@theaninova.de>
2023-04-20 12:22:55 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,79 @@
+<div class="scroll">
+  <div class="parallax">
+    <article>
+      <h1>Welcome to SvelteKit</h1>
+      {#each Array.from({length: 10}) as _}
+        <button on:click={() => alert("Click!")}>Click me!</button>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </p>
+        {/each}
+    </article>
+  </div>
+</div>
+
+<style lang="scss">
+  $parallax-background: gray;
+  $background: black;
+  $parallax-strength: 2;
+  $overscroll-padding: 720px;
+  $content-size: 230px;
+
+  :global(:root) {
+    font-family: sans-serif;
+    overflow: hidden;
+  }
+
+  :global(body) {
+    margin: 0;
+    background: $background;
+  }
+
+  .scroll {
+    height: 200vh;
+    overflow-y: auto;
+    overflow-x: hidden;
+
+    perspective: 2px;
+    perspective-origin: center top;
+  }
+
+  .parallax {
+    transform-style: preserve-3d;
+    position: relative;
+
+    &::after {
+      content: ' ';
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+
+      $height: calc($content-size + $overscroll-padding);
+      $translateY: calc($overscroll-padding * $parallax-strength);
+      $translateZ: calc(-1px * $parallax-strength);
+      $transform-origin: calc($parallax-strength * $parallax-strength * $overscroll-padding);
+
+      height: $height;
+      width: 150%;
+      transform-origin: 50% $transform-origin;
+      translate: 0px $translateY $translateZ;
+      scale: $parallax-strength;
+      z-index: -1;
+
+      background: $parallax-background;
+    }
+  }
+
+  article {
+    margin-block: 50px;
+    margin-inline: 64px;
+    padding: 16px;
+    background: rgba(255, 255, 255, 0.7);
+    height: 500vh;
+  }
+</style>
---
+++ src/routes/3d/+page.svelte
@@ -0,0 +1,78 @@
+<article>
+  <h1>
+    3D
+  </h1>
+
+  <div class="container">
+    <section>
+      <h2>No pseudo elements</h2>
+      <div class="depth-container scroll">
+        <div class="rotate-1">div</div>
+        <div class="rotate-2">div</div>
+        <div class="long"></div>
+      </div>
+    </section>
+    <section>
+      <h2>With one pseudo element</h2>
+      <div class="depth-container scroll pseudo">
+        <div class="rotate-1">div</div>
+        <div class="long"></div>
+      </div>
+    </section>
+  </div>
+</article>
+
+<style lang="scss" global>
+  :root {
+    overflow: hidden;
+  }
+
+  .container {
+    display: flex;
+    gap: 8px;
+  }
+
+  .scroll {
+    overflow-y: scroll;
+  }
+
+  .long {
+    height: 400px;
+  }
+
+  .depth-container {
+    outline: 1px solid black;
+
+    height: 300px;
+    width: 300px;
+
+    perspective: 100px;
+    transform-style: preserve-3d;
+  }
+
+  .pseudo::after {
+    content: '::after';
+  }
+
+  .rotate-1, .rotate-2, .pseudo::after {
+    opacity: 0.8;
+
+    position: absolute;
+    top: 100px;
+    left: 100px;
+
+    width: 100px;
+    height: 100px;
+  }
+
+  .rotate-1 {
+    background-color: red;
+    transform: rotate3d(0, 1, 0, 30deg);
+  }
+
+  .pseudo::after,
+  .rotate-2 {
+    background-color: green;
+    transform: rotate3d(0, 1, 0, -30deg);
+  }
+</style>
